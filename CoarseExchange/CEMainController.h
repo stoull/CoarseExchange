d1159f50cf2750e89e6c93b131d465a4796a4f10
Chencc <chencc@linkapp.cn>
2015-12-16 18:09:18 +0800

    add Login XMPP

---
+++ CoarseExchange/CEMainController.h
@@ -0,0 +1,13 @@
+//
+//  CEMainController.h
+//  CoarseExchange
+//
+//  Created by stoull on 12/16/15.
+//  Copyright © 2015 AChang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CEMainController : UITabBarController
+
+@end
